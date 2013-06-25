246a92fb01a7ce4b378ec17a767c07217d4e2ca0
Charalampos Emmanouilidis <charalampos.emmanouilidis@philips.com>
2013-06-25 14:40:29 +0200

    upate background color

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ glist_append string /apps/gnome-terminal/global/profile_list "$PROFILE_SLUG"
 gset string visible_name "$PROFILE_NAME"
 
 gset string palette	"#1C1C1D1D1919:#D0D01B1A2424:#A7A7D3D32C2C:#D8D8CFCF6767:#6161B8B8D0D0:#69695A5ABBBB:#D5D538386464:#FEFEFFFFFEFE:#1C1C1D1D1919:#D1D12A292423:#A7A7D3D32C2C:#D8D8CFCF6767:#6161B8B8D0D0:#69695A5ABBBB:#D5D538386464:#FEFEFFFFFEFE"
-gset string background_color "#1D1D1D1D1919"
+gset string background_color "#232325252626"
 gset string foreground_color "#F6F6F5F5EEEE"
 gset string bold_color "#FF99FF88FF55"
 gset bool   bold_color_same_as_fg "true"
